23a80798c50ec6333532920ad24cb16a88511161
Rafael Ribeiro <rafael.ribeiro@cargosapiens.com>
2025-05-13 16:45:09 -0300

    Obrigatório 2

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function (Request $request) {
+
+    return [
+        'message' => 'Fullstack Challenge 🏅 - Dictionary',
+    ];
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
